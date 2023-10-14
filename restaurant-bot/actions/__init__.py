401589898711325903ca2e9ddb6845d885ca5df7
Gao Yuan <143201563+YvonneGaoY@users.noreply.github.com>
2023-10-14 19:53:54 +0800

    Add files via upload

---
+++ restaurant-bot/actions/__init__.py
@@ -0,0 +1,77 @@
+from datetime import datetime, timedelta
+
+# 模拟一些位置信息
+LOCATIONS = [
+    "Haidian District, Beijing",
+    "Minhang District, Shanghai",
+    "Xuhui District, Shanghai"
+]
+
+# Simulated restaurant and menu information
+RESTANRANTS = {
+    "Pizza Hut": {
+        "location": "Haidian District, Beijing",
+        "menu": ["Pizza", "Salad", "steak"]
+    },
+    "McDonald's": {
+        "location": "Minhang District, Shanghai",
+        "menu": ["Burger", "fried chicken", "Milkshake"]
+    },
+    "Hidden aquatic products": {
+        "location": "Shanghai Xuhui District ",
+        "menu": ["Sushi", "Salmon Sashimi", " Eel"]
+    },
+    "Yuanjia": {
+        "location": "Shanghai Xuhui District ",
+        "menu": ["Bibimbap", "cold noodles", "Beef platter"]
+    },
+}
+
+
+class OrderManager:
+    def __init__(self):
+        self.orders = {}
+        self.orders_prefix = "orderid"
+        self.orders_num = 1
+
+    # def create_order(self, order_id, items, create_time):
+    def create_order(self, items, create_time):
+        order_id = f"{self.orders_prefix}{self.orders_num}"
+        self.orders_num += 1
+        if order_id not in self.orders:
+            self.orders[order_id] = {
+                "Meal": items, "Status": "Preparing", "Creation time": create_time}
+        else:
+            return None
+        return order_id
+
+    def get_order_status(self, order_id, current_time_str):
+        oid_string = ', '.join(self.orders.keys())
+        print(f"[OrderManager] get_order_status||{oid_string}")
+        if order_id in self.orders:
+            # 将字符串解析为 datetime 对象
+            create_time = datetime.strptime(
+                self.orders[order_id]["Creation time"], "%Y-%m-%d %H:%M:%S")
+            current_time = datetime.strptime(
+                current_time_str, "%Y-%m-%d %H:%M:%S")
+            # 计算两个 datetime 对象之间的时间差
+            time_difference = current_time - create_time
+
+            if time_difference > timedelta(minutes=1):
+                self.orders[order_id]["Status"] = "Finished"
+            return self.orders[order_id]["Status"]
+        else:
+            return None
+
+    def delete_order(self, order_id):
+        oid_string = ', '.join(self.orders.keys())
+        print(f"[OrderManager] delete_order||{oid_string}")
+        if order_id in self.orders:
+            del self.orders[order_id]
+        else:
+            return None
+        return "success"
+
+
+# 创建订单管理器实例
+ORDER_MANAGER = OrderManager()
---
+++ restaurant-bot/actions/actions.py
@@ -0,0 +1,189 @@
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+from typing import Text, List, Dict, Any
+from . import LOCATIONS, RESTANRANTS, ORDER_MANAGER
+
+import datetime
+import uuid
+
+
+class ActionSearchRestaurant(Action):
+    def name(self) -> Text:
+        return "action_find_restaurant"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        try:
+            location = tracker.get_slot("location")
+            log_time = datetime.datetime.now()
+            log_time_str = log_time.strftime("%Y-%m-%d %H:%M:%S")
+            print(f"{log_time_str}||action_find_restaurant||location:{location}")
+            if location == None:
+                error_message = "Excuse me, where would you like to go to a restaurant near here?"
+                dispatcher.utter_message(error_message)
+                return []
+
+            # 根据用户提供的位置查找餐厅信息
+            if location in LOCATIONS:
+                # print(f"Location: {location}")
+                # print("Restaurants:")
+                # for restaurant, info in RESTANRANTS.items():
+                #     print(restaurant, info["location"])
+                nearby_restaurants = [restaurant for restaurant, info in RESTANRANTS.items(
+                ) if info["location"] == location]
+                if nearby_restaurants:
+                    restaurant_list = "\n".join(nearby_restaurants)
+                    response_message = f"Here are some nearby restaurants: \n{restaurant_list}"
+                else:
+                    response_message = "No restaurants nearby"
+            else:
+                response_message = "Sorry, the location you provided could not be found."
+            
+            dispatcher.utter_message(response_message)
+        except Exception as _:
+            error_message = "Sorry, there are some problems, please try again later."
+            dispatcher.utter_message(error_message)
+        return []
+
+class ActionBrowseMenu(Action):
+    def name(self) -> Text:
+        return "action_view_menu"
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        try:
+            restaurant = tracker.get_slot("restaurant")
+            if restaurant == None:
+                error_message = "Excuse me, which restaurant would you like to have dinner at?"
+                dispatcher.utter_message(error_message)
+                return []
+            if restaurant in RESTANRANTS:
+                memu = RESTANRANTS[restaurant]["menu"]
+                if memu:
+                    memu_list = "\n".join(memu)
+                response_message = f"Here is the menu for {restaurant}:\n{memu_list}"
+            else:
+                response_message = "Sorry, the restaurant you provided cannot be found."
+            
+            dispatcher.utter_message(response_message)
+        except Exception as _:
+            error_message = "Sorry, there are some problems, please try again later."
+            dispatcher.utter_message(error_message)
+        return []
+
+class ActionOrderDish(Action):
+    def name(self) -> Text:
+        return "action_order_dish"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        try:
+            dish = tracker.get_slot("dish")
+            restaurant = tracker.get_slot("restaurant")
+            log_time = datetime.datetime.now()
+            log_time_str = log_time.strftime("%Y-%m-%d %H:%M:%S")
+            print(f"{log_time_str}||action_order_dish||restaurant:{restaurant}||dish:{dish}")
+            if restaurant is None:
+                error_message = "Excuse me, where are you near?"
+                dispatcher.utter_message(error_message)
+                return []
+            if dish is None:
+                error_message = "Excuse me, what kind of food would you like to have?"
+                dispatcher.utter_message(error_message)
+                return []
+            if restaurant in RESTANRANTS:
+                memu = RESTANRANTS[restaurant]["menu"]
+                if dish in memu:
+                    # order_id = uuid.uuid4()
+                    # bytes_uuid = order_id.bytes
+                    # # 将字节表示形式转换为十六进制字符串，无连字符
+                    # hex_uuid = ''.join('{:02x}'.format(b) for b in bytes_uuid)
+
+                    # order_id_str = str(hex_uuid)
+
+                    current_datetime = datetime.datetime.now()
+                    # 将 datetime 对象转换为字符串
+                    date_time_str = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+                    id = ORDER_MANAGER.create_order(dish, date_time_str)
+                    if id is None:         
+                        error_message = "Sorry, there are some problems, please try again later."
+                        dispatcher.utter_message(error_message)
+                        return []
+                    else: 
+                        response_message = f"You ordered a {dish} from {date_time_str} at {restaurant}, please pick up your order number: {id}."
+                else:
+                    response_message = "Sorry, it's not on the menu."
+            else:
+                response_message = "Sorry, the restaurant you provided cannot be found."
+
+            dispatcher.utter_message(response_message)
+        except Exception as _:
+            error_message = "Sorry, there are some problems, please try again later."
+            dispatcher.utter_message(error_message)
+        return []
+
+class ActionCancelOrder(Action):
+    def name(self) -> Text:
+        return "action_cancel_order"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        try:
+            # 取消订单的逻辑
+            # 可以从数据库中查找用户的订单并取消
+            oid = tracker.get_slot("oid")
+            log_time = datetime.datetime.now()
+            log_time_str = log_time.strftime("%Y-%m-%d %H:%M:%S")
+            print(f"{log_time_str}||action_cancel_order||order_id:{oid}")
+            if oid == None:
+                error_message = "Excuse me, which order would you like to cancel?"
+                dispatcher.utter_message(error_message)
+                return []
+            status = ORDER_MANAGER.delete_order(oid)
+            if status == "success":
+                response_message = f"Your order: {oid} cancelled."
+            else:
+                response_message = "Sorry, this order cannot be found. Sorry, this order cannot be found."
+            dispatcher.utter_message(response_message)
+        except Exception as _:
+            error_message = "Sorry, there are some problems, please try again later."
+            dispatcher.utter_message(error_message)
+        return []
+
+class ActionTrackOrder(Action):
+    def name(self) -> Text:
+        return "action_track_order"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        try:
+            # 跟踪订单的逻辑
+            # 可以从数据库中查找用户的订单状态
+            oid = tracker.get_slot("oid")
+            log_time = datetime.datetime.now()
+            log_time_str = log_time.strftime("%Y-%m-%d %H:%M:%S")
+            print(f"{log_time_str}||action_track_order||order_id:{oid}")
+            if oid == None:
+                error_message = "Excuse me, which order would you like to inquire about?"
+                dispatcher.utter_message(error_message)
+                return []
+            current_datetime = datetime.datetime.now()
+            date_time_str = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+            status = ORDER_MANAGER.get_order_status(oid, date_time_str)
+            if status is None:
+                response_message = "Sorry, this order cannot be found."
+            else:
+                response_message = f"The status of your order is:{status}."
+            dispatcher.utter_message(response_message)
+        except Exception as _:
+            error_message = "Sorry, there are some problems, please try again later."
+            dispatcher.utter_message(error_message)
+        return []
